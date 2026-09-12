18ff89b3428364bc5251b9fa1d01fbb485dd37fd
yerraxd <liliboymak@gmail.com>
2026-09-13 01:02:35 +0500

    Add Season enum and wardrobeItems constants

---
+++ src/outfitbuilder/wardrobeItems.java
@@ -0,0 +1,17 @@
+package outfitbuilder;
+
+public final class wardrobeItems {
+    private wardrobeItems() {
+    }
+
+    public static final String T_SHIRT = "T-Shirt";
+    public static final String JEANS = "Jeans";
+    public static final String SNEAKERS = "Sneakers";
+    public static final String CAP = "Cap";
+
+    public static final String DRESS_SHIRT = "Dress Shirt";
+    public static final String SUIT_TROUSERS = "Suit Trousers";
+    public static final String OXFORD_SHOES = "Oxford Shoes";
+    public static final String BLAZER = "Blazer";
+    public static final String TIE = "Tie";
+}
