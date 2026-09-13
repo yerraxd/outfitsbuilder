de187690d34eb26648e9988b60d1e8e8cc7bdcaf
yerraxd <liliboymak@gmail.com>
2026-09-13 08:29:51 +0500

    Added casual outfit builder with a check for unsuitable footwear and formal outfit builder with a required tie check

---
+++ src/outfitbuilder/formalOutfitbuilder.java
@@ -0,0 +1,17 @@
+package outfitbuilder;
+
+public class formalOutfitbuilder extends abstractOutfitbuilder {
+
+    @Override
+    public outfit build() {
+        if (!accessories.contains(wardrobeItems.TIE)) {
+            throw new IllegalStateException("A formal outfit requires a tie.");
+        }
+        return super.build();
+    }
+
+    @Override
+    protected String getStyleName() {
+        return "Formal";
+    }
+}
